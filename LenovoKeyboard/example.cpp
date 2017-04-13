6391651eeb33bbde3847a2ca363e8acb7d01ec2b
ouahib-el-hanchi <ouahib.el.hanchi@gmail.com>
2017-04-13 23:38:27 +0100

    Added exception handling

--- LenovoKeyboard/example.cpp
+++ LenovoKeyboard/example.cpp
@@ -4,17 +4,25 @@
 
 int main()
 {
-	auto & keyboard = Lenovo::Keyboard::getInstance();
+	try {
+		auto & keyboard = Lenovo::Keyboard::getInstance();
 
-	std::cout << std::boolalpha;
+		std::cout << std::boolalpha;
+		
+		std::cout << "[-] Backlight is turned on: " << keyboard.isFnLockOn() << std::endl;
+		keyboard.toggleBacklight();
+		std::cout << "[-] Backlight is turned off: " << keyboard.isFnLockOff() << std::endl;
 
-	std::cout << "[-] Backlight is turned on: " << keyboard.isFnLockOn() << std::endl;
-	keyboard.toggleBacklight();
-	std::cout << "[-] Backlight is turned off: " << keyboard.isFnLockOff() << std::endl;
-
-	std::cout << "[-] Function keys lock is turned on: " << keyboard.isFnLockOn() << std::endl;
-	keyboard.toggleFnLock();
-	std::cout << "[-] Function keys lock is turned off: " << keyboard.isFnLockOff() << std::endl;
+		std::cout << "[-] Function keys lock is turned on: " << keyboard.isFnLockOn() << std::endl;
+		keyboard.toggleFnLock();
+		std::cout << "[-] Function keys lock is turned off: " << keyboard.isFnLockOff() << std::endl;
+	}
+	catch (const Lenovo::UnsupportedKeyboard & error) {
+		std::cerr << "*** " << error.what() << std::endl;
+	}
+	catch (const Lenovo::Error & error) {
+		std::cerr << "*** " << error.what() << std::endl;
+	}
 
 	(void) std::getchar();
 
